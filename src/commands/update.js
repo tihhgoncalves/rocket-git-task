d50f2c2d27f3c430ea96f0b970238b2525be6cbe
Tihh Gonçalves <tihhgoncalves@gmail.com>
2025-04-08 17:18:08 -0300

    🚀 Deploy da task/update-com-merge

--- src/commands/update.js
+++ src/commands/update.js
@@ -3,10 +3,12 @@ const log = require('../utils/log');
 const { getBranches } = require('../config');
 
 module.exports = async () => {
+
+    // seleciona branchs
     const currentBranch = git.getCurrentBranch();
     const { prodBranch } = getBranches();
 
-    // Verifica se está em uma task
+    // Verifica se está em uma task (obrigatório)
     if (!currentBranch.startsWith('task/')) {
         log.error(`O comando "update" só pode ser executado em uma branch de task.`);
         log.error(`Você está na branch "${currentBranch}".`);
@@ -19,21 +21,24 @@ module.exports = async () => {
     log.info(`Atualizando a task "${currentBranch}" com as últimas alterações de "${prodBranch}"...`);
 
     try {
-        // Garante que branch está atualizado antes do rebase
+        // Atualiza a branch de produção local com a versão mais recente
+        git.run('git fetch origin');
+        
+        // Garante que branch de PROD está atualizado antes do merge
         git.checkout(prodBranch);
         git.pull();
 
         // Volta pra task atual e executa o rebase
         git.checkout(currentBranch);
-        git.run(`git rebase ${prodBranch}`);
+
+        // Faz o merge com mensagem customizada
+        git.run(`git merge origin/${prodBranch} -m "update: sincronizando com '${prodBranch}'"`);
 
         log.success(`Task "${currentBranch}" atualizada com sucesso!`);
+        
     } catch (error) {
         log.error(`Falha ao atualizar a task "${currentBranch}".`);
         log.error(`Erro: ${error.message}`);
         process.exit(1);
-    } finally {
-        // garante que no final sempre volta pra branch original
-        git.checkout(currentBranch);   
     }
 };
