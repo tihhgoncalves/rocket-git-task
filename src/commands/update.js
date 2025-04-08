0886d71b00f896962d59f55e8c2b3961807083b6
Tihh Gonçalves <tihhgoncalves@gmail.com>
2025-04-08 17:16:23 -0300

    Atualiza lógica de atualização da task para usar merge ao invés de rebase e melhora os comentários no código

--- src/commands/update.js
+++ src/commands/update.js
@@ -19,21 +19,23 @@ module.exports = async () => {
     log.info(`Atualizando a task "${currentBranch}" com as últimas alterações de "${prodBranch}"...`);
 
     try {
-        // Garante que branch está atualizado antes do rebase
+        // Atualiza a branch de produção local com a versão mais recente
+        git.run('git fetch origin');
+        
+        // Garante que branch de PROD está atualizado antes do merge
         git.checkout(prodBranch);
         git.pull();
 
         // Volta pra task atual e executa o rebase
         git.checkout(currentBranch);
-        git.run(`git rebase ${prodBranch}`);
+
+        // Faz o merge com mensagem customizada
+        git.run(`git merge origin/${prodBranch} -m "update: sincronizando com '${prodBranch}'"`);
 
         log.success(`Task "${currentBranch}" atualizada com sucesso!`);
     } catch (error) {
         log.error(`Falha ao atualizar a task "${currentBranch}".`);
         log.error(`Erro: ${error.message}`);
         process.exit(1);
-    } finally {
-        // garante que no final sempre volta pra branch original
-        git.checkout(currentBranch);   
     }
 };
