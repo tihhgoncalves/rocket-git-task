08319b287640558cb6de0fa019d466092fe8d5ff
Tihh Gonçalves <tihhgoncalves@gmail.com>
2025-08-15 19:48:14 -0300

    Squashed commit of the following:
    commit 0209c7525d164f76ba9b5ca4c8fecbbc234065d6
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 19:43:40 2025 -0300
    
        Ajustando index.js pra aceitar as versões como parametro
    
    commit ea1df2e0cd97833315d7be4dbfbabe41f50fb770
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 19:41:30 2025 -0300
    
        Ajustando deploys pra releases
    
    commit 5e1d60fd7f95c564a964bd8b14498cb0e4a5cd02
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 19:24:03 2025 -0300
    
        ajustando mensagens de console
    
    commit 043bebcb64f280f33410d585459ba9fe0cd82964
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 19:06:49 2025 -0300
    
        correção
    
    commit 9b5a25b37d82637e09289024bc84b03207315793
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 19:03:02 2025 -0300
    
        up
    
    commit f545fd2138e0ed9e6e1b96cce59adc300a2097e7
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 19:02:31 2025 -0300
    
        Não envia o tag agora
    
    commit dc8bd9e5765e5be050d3329aff860615083f5da5
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 18:58:00 2025 -0300
    
        Adicionando informação de branch
    
    commit b9d6c6805b3b7a8c618222e1c122e8fe6e2655d6
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 18:54:08 2025 -0300
    
        Forçando enviar as modificações do release
    
    commit fab6f9bd57bdd17ff31f305f10fe3548d1001b27
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Fri Aug 15 18:49:58 2025 -0300
    
        Commitando ajustes de release
    
    commit 46f59ce7015591f879a119a56a7866238226264f
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Thu Apr 24 13:44:47 2025 -0300
    
        🔖 Bump versão para 1.7.13 (voltando a 1.7.8)

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -1,12 +1,15 @@
 const git = require('../utils/git');
 const log = require('../utils/log');
-const { getBranches } = require('../config');
 
 module.exports = async ({ target }) => {
     const currentBranch = git.getCurrentBranch();
-    const { devBranch, prodBranch } = getBranches();
 
-    const targetBranch = target === 'production' ? prodBranch : devBranch;
+    // Só aceita deploy para release/X.Y.Z ou release/X.Y.Z-beta.N
+    if (!/^\d+\.\d+\.\d+(-beta\.\d+)?$/.test(target)) {
+        log.error(`Destino de deploy inválido: "${target}". Informe apenas a versão, exemplo: git-task deploy 1.2.3 ou git-task deploy 1.2.3-beta.1`);
+        process.exit(1);
+    }
+    const targetBranch = `release/${target}`;
 
     // Verifica se está em uma branch de task
     if (!currentBranch.startsWith('task/')) {
@@ -21,31 +24,31 @@ module.exports = async ({ target }) => {
     log.info(`Fazendo deploy da task "${currentBranch}" para "${targetBranch}"...`);
 
     try {
-      // Checkout na branch de destino (develop ou production)
-      git.checkout(targetBranch);
-      git.pull();
+      // Checkout na branch do release
+        git.checkout(targetBranch);
+        git.pull();
 
       // Faz o merge com squash, permitindo conflitos
-      log.info(`Preparando o merge da task "${currentBranch}" para "${targetBranch}"...`);
-      git.run(`git merge --squash ${currentBranch}`);
+        log.info(`Preparando o merge da task "${currentBranch}" para "${targetBranch}"...`);
+        git.run(`git merge --squash ${currentBranch}`);
 
       // Faz o commit com mensagem personalizada
-      git.run(
-        `git commit -m "🚀 Deploy da task '${currentBranch}' para ${targetBranch}"`
-      );
+        git.run(
+            `git commit -m "🚀 Deploy da task '${currentBranch}' para ${targetBranch}"`
+        );
 
-      git.push(targetBranch);
+        git.push(targetBranch);
 
       // Volta para a branch original
-      git.checkout(currentBranch);
+        git.checkout(currentBranch);
 
-      log.success(`Deploy da task "${currentBranch}" concluído com sucesso!`);
+        log.success(`Deploy da task "${currentBranch}" concluído com sucesso!`);
     } catch (error) {
         log.error(`Falha ao fazer deploy da task "${currentBranch}".`);
         log.error(`Erro: ${error.message}`);
         process.exit(1);
     } finally {
         // garante que no final sempre volta pra branch original
-        git.checkout(currentBranch);   
+        git.checkout(currentBranch);
     }
 };
